57f8c89b91241c7ac73f8ad48478e51d414109b2
Callum Jones <callum72gj19@gmail.com>
2022-09-12 17:39:09 +0100

    Began work on outfits.
    Began experimenting with adding vanity outfits. Created sprites and basic items for an outfit based on the one worn by Ruby Rose.

---
+++ Items/Armor/RoseBoots.cs
@@ -0,0 +1,28 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace TRRA.Items.Armor
+{
+	[AutoloadEquip(EquipType.Legs)]
+	public class RoseBoots : ModItem
+	{
+		public override void SetStaticDefaults() {
+			DisplayName.SetDefault("Rose Boots");
+			Tooltip.SetDefault("This is a modded leg armor.");
+		}
+
+		public override void SetDefaults()
+		{
+			Item.width = 18;
+			Item.height = 18;
+			Item.rare = ItemRarityID.Blue;
+			Item.vanity = true;
+		}
+
+		public override void AddRecipes() => CreateRecipe()
+			.AddIngredient(ItemID.Silk, 1)
+			.AddTile(TileID.Loom)
+			.Register();
+	}
+}
---
+++ Items/Armor/RoseCloak.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TRRA.Items.Armor
+{
+	[AutoloadEquip(EquipType.Body, EquipType.Back)]
+	public class RoseCloak : ModItem
+	{
+		public override void SetStaticDefaults() {
+			DisplayName.SetDefault("Rose Cloak");
+			Tooltip.SetDefault("This is a modded body armor.");
+		}
+
+		public override void SetDefaults()
+		{
+			Item.width = 18;
+			Item.height = 14;
+			Item.rare = ItemRarityID.Blue;
+			Item.vanity = true;
+		}
+
+        public override void DrawArmorColor(Player drawPlayer, float shadow, ref Color color, ref int glowMask, ref Color glowMaskColor)
+        {
+            base.DrawArmorColor(drawPlayer, shadow, ref color, ref glowMask, ref glowMaskColor);
+			drawPlayer.back = Item.backSlot;
+		}
+
+        public override void AddRecipes() => CreateRecipe()
+			.AddIngredient(ItemID.Silk, 1)
+			.AddTile(TileID.Loom)
+			.Register();
+	}
+}
