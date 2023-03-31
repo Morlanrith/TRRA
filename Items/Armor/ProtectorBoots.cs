4f53fca08da0cc6b7b99e544cffd5517848d5a42
Callum Jones <44137340+Morlanrith@users.noreply.github.com>
2023-03-31 15:28:33 +0100

    Added the Protector set
    Added the Protector vanity set, which is based on the Volume 7 outfit worn by Penny Polendina.

---
+++ Items/Armor/ProtectorBoots.cs
@@ -0,0 +1,32 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace TRRA.Items.Armor
+{
+	[AutoloadEquip(EquipType.Legs)]
+	public class ProtectorBoots : ModItem
+	{
+		public override void SetStaticDefaults() {
+			DisplayName.SetDefault("Protector Boots");
+			Tooltip.SetDefault("'Take a message of hope to the stars'");
+			Terraria.GameContent.Creative.CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+		}
+
+		public override void SetDefaults()
+		{
+			Item.width = 12;
+			Item.height = 12;
+			Item.rare = ItemRarityID.Blue;
+			Item.value = Item.sellPrice(silver: 15);
+			Item.vanity = true;
+		}
+
+		public override void AddRecipes() => CreateRecipe()
+            .AddIngredient(ItemID.Nanites, 1)
+            .AddIngredient(ItemID.BlackPaint, 1)
+            .AddRecipeGroup("IronBar", 5)
+            .AddTile(TileID.Anvils)
+            .Register();
+	}
+}
---
+++ Items/Armor/ProtectorTop.cs
@@ -0,0 +1,31 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TRRA.Items.Armor
+{
+	[AutoloadEquip(EquipType.Body)]
+	public class ProtectorTop : ModItem
+	{
+		public override void SetStaticDefaults() {
+			DisplayName.SetDefault("Protector Top");
+			Tooltip.SetDefault("'Feels as warm as a hug'");
+			Terraria.GameContent.Creative.CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+		}
+
+		public override void SetDefaults()
+		{
+			Item.width = 22;
+			Item.height = 18;
+			Item.rare = ItemRarityID.Blue;
+			Item.value = Item.sellPrice(silver: 15);
+			Item.vanity = true;
+		}
+
+        public override void AddRecipes() => CreateRecipe()
+			.AddIngredient(ItemID.Nanites, 1)
+			.AddIngredient(ItemID.Silk, 20)
+			.AddTile(TileID.Loom)
+			.Register();
+	}
+}
