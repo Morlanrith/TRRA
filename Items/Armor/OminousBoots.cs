4908322eeb23adcc3bb49fe488d968a9efbf1ea1
Callum Jones <44137340+Morlanrith@users.noreply.github.com>
2023-04-01 13:25:29 +0100

    Added the Ominous set
    Added the Ominous vanity set, which is based on the original outfit worn by Raven Branwen.

---
+++ Items/Armor/OminousBoots.cs
@@ -0,0 +1,31 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace TRRA.Items.Armor
+{
+	[AutoloadEquip(EquipType.Legs)]
+	public class OminousBoots : ModItem
+	{
+		public override void SetStaticDefaults() {
+			DisplayName.SetDefault("Ominous Boots");
+			Tooltip.SetDefault("'I won't fall for you'");
+			Terraria.GameContent.Creative.CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+		}
+
+		public override void SetDefaults()
+		{
+			Item.width = 12;
+			Item.height = 12;
+			Item.rare = ItemRarityID.Blue;
+			Item.value = Item.sellPrice(silver: 15);
+			Item.vanity = true;
+		}
+
+		public override void AddRecipes() => CreateRecipe()
+			.AddIngredient(ItemID.Deathweed, 1)
+			.AddIngredient(ItemID.Silk, 20)
+            .AddTile(TileID.Loom)
+            .Register();
+	}
+}
---
+++ Items/Armor/OminousMask.cs
@@ -0,0 +1,33 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace TRRA.Items.Armor
+{
+	[AutoloadEquip(EquipType.Head)]
+	public class OminousMask : ModItem
+	{
+		public override void SetStaticDefaults() {
+			DisplayName.SetDefault("Ominous Mask");
+			Tooltip.SetDefault("'Close your eyes now time for dreams'");
+			Terraria.GameContent.Creative.CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+
+			ArmorIDs.Head.Sets.DrawFullHair[Item.headSlot] = true;
+		}
+
+		public override void SetDefaults() {
+			Item.width = 18;
+			Item.height = 18;
+			Item.rare = ItemRarityID.Blue;
+			Item.value = Item.sellPrice(silver: 15);
+			Item.vanity = true;
+		}
+
+
+		public override void AddRecipes() => CreateRecipe()
+			.AddIngredient(ItemID.Deathweed, 1)
+			.AddIngredient(ItemID.Bone, 5)
+			.AddTile(TileID.WorkBenches)
+			.Register();
+	}
+}
