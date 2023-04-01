1a82d8bedfb1911d66eea81351c2c21bc815fc91
Callum Jones <44137340+Morlanrith@users.noreply.github.com>
2023-04-01 09:40:22 +0100

    Added the Unlucky set
    Added the Unlucky vanity set, which is based on the original outfit worn by Qrow Branwen.

---
+++ Items/Armor/UnluckyPants.cs
@@ -0,0 +1,31 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace TRRA.Items.Armor
+{
+	[AutoloadEquip(EquipType.Legs)]
+	public class UnluckyPants : ModItem
+	{
+		public override void SetStaticDefaults() {
+			DisplayName.SetDefault("Unlucky Pants");
+			Tooltip.SetDefault("'I'm a mirror broken'");
+			Terraria.GameContent.Creative.CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+		}
+
+		public override void SetDefaults()
+		{
+			Item.width = 18;
+			Item.height = 18;
+			Item.rare = ItemRarityID.Blue;
+			Item.value = Item.sellPrice(silver: 15);
+			Item.vanity = true;
+		}
+
+		public override void AddRecipes() => CreateRecipe()
+			.AddIngredient(ItemID.Blinkroot, 1)
+			.AddIngredient(ItemID.Silk, 20)
+			.AddTile(TileID.Loom)
+			.Register();
+	}
+}
---
+++ Items/Armor/UnluckyTop.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TRRA.Items.Armor
+{
+	[AutoloadEquip(EquipType.Body, EquipType.Back)]
+	public class UnluckyTop : ModItem
+	{
+		public override void SetStaticDefaults() {
+			DisplayName.SetDefault("Unlucky Top");
+			Tooltip.SetDefault("'I'm an albatross'");
+			Terraria.GameContent.Creative.CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+		}
+
+		public override void SetDefaults()
+		{
+			Item.width = 22;
+			Item.height = 18;
+			Item.rare = ItemRarityID.Blue;
+			Item.value = Item.sellPrice(silver: 15);
+			Item.vanity = true;
+		}
+
+        public override void DrawArmorColor(Player drawPlayer, float shadow, ref Color color, ref int glowMask, ref Color glowMaskColor)
+        {
+            base.DrawArmorColor(drawPlayer, shadow, ref color, ref glowMask, ref glowMaskColor);
+			drawPlayer.back = Item.backSlot;
+		}
+
+        public override void AddRecipes() => CreateRecipe()
+			.AddIngredient(ItemID.Blinkroot, 1)
+			.AddIngredient(ItemID.Silk, 20)
+			.AddTile(TileID.Loom)
+			.Register();
+	}
+}
